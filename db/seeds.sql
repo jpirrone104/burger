32b40c600ae53b5d9c33d5c48cccea7b87cdf184
Jen Pirrone <pirronej104@gmail.com>
2019-02-23 18:41:17 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beyond Burger", false)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", false)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", false)
